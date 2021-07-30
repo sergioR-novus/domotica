bba2cc67b2fea6ac5b112d3355e0d36c51e4f944
sergioR-novus <sergio@aquaexploracion.mx>
2021-07-30 13:58:48 -0500

    x

--- app/ms5837-python/test.py
+++ app/ms5837-python/test.py
@@ -8,6 +8,9 @@ sensor = ms5837.MS5837_30BA() # Default I2C bus is 1 (Raspberry Pi 3)
 #sensor = ms5837.MS5837_02BA(0)
 #sensor = ms5837.MS5837(model=ms5837.MS5837_MODEL_30BA, bus=0) # Specify model and bus
 
+currentstate = 0
+previousstate = 0
+
 # We must initialize the sensor before reading it
 if not sensor.init():
         print("Sensor could not be initialized")
@@ -42,11 +45,57 @@ time.sleep(5)
 # Spew readings
 while True:
         if sensor.read():
-                print("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F") % (
-                sensor.pressure(), # Default is mbar (no arguments)
-                sensor.pressure(ms5837.UNITS_psi), # Request psi
-                sensor.temperature(), # Default is degrees C (no arguments)
-                sensor.temperature(ms5837.UNITS_Farenheit)) # Request Farenheit
+                #Logica y POST
+                try:
+                        print("BANDERA")
+                        
+                        # # Loop until PIR output is 0
+                        # while GPIO.input(pinpir) == 1:
+                        
+                        #         currentstate = 0
+
+                        # print("    Ready")
+                        
+                        # Loop until users quits with CTRL-C
+                        while True:
+                        
+                                # Read PIR state
+                                print("T: %0.2f C") % (sensor.temperature())
+
+                                if sensor.temperature() > 28:
+                                        currentstate = 1
+                                else: 
+                                        currentstate = 0
+
+                                # If the PIR is triggered
+                                if currentstate == 1 and previousstate == 0:
+                                
+                                        print("Motion detected!")
+                                        
+                                        # Your IFTTT URL with event name, key and json parameters (values)
+                                        r = requests.post('https://maker.ifttt.com/trigger/motion_detected/with/key/LN2n2aZaCLhO6u2IygYbV', params={"value1":"none","value2":"none","value3":"none"})
+                                        
+                                        # Record new previous state
+                                        previousstate = 1
+                                        
+                                        #Wait 120 seconds before looping again
+                                        print("Waiting 120 seconds")
+                                        time.sleep(120)
+                                        
+                                # If the PIR has returned to ready state
+                                elif currentstate == 0 and previousstate == 1:
+                                
+                                        print("Ready")
+                                        previousstate = 0
+
+                                # Wait for 10 milliseconds
+                                time.sleep(0.01)
+
+                except KeyboardInterrupt:
+                        print("    Quit")
+                                
         else:
                 print("Sensor read failed!")
                 exit(1)
+
+
