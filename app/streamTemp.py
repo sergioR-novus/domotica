53245bd84f86ea4b36ea027fe879482080b71db1
sergioR-novus <sergio@aquaexploracion.mx>
2021-08-01 18:47:00 -0500

    pir

--- app/streamTemp.py
+++ app/streamTemp.py
@@ -1,16 +1,22 @@
+import RPi.GPIO as GPIO
 import time
+import requests
 from w1thermsensor import W1ThermSensor
 from ISStreamer.Streamer import Streamer
+
 sensor = W1ThermSensor()
+pinpir = 17
+GPIO.setup(pinpir, GPIO.IN)
+currentstate = 0
+previousstate = 0
 
 
 # --------- User Settings ---------
 SENSOR_LOCATION_NAME = "Casa"
 BUCKET_NAME = ":partly_sunny: Room stats"
-BUCKET_KEY = "temp"
+BUCKET_KEY = "test"
 ACCESS_KEY = "ist_ENpoPBXw1ubY9eAaNld52lkMMLpjKAgS"
 MINUTES_BETWEEN_READS = .05
-METRIC_UNITS = True
 # ---------------------------------
 
 streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
@@ -19,15 +25,35 @@ streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=A
 while True:
         try:
                 temp_c = sensor.get_temperature()
+                streamer.log(SENSOR_LOCATION_NAME + " Temperature(C)", temp_c)
+                print(temp_c)  
+
+                print("Waiting for PIR to settle ...")
+                while GPIO.input(pinpir) == 1:
+                        currentstate = 0
+                        print(GPIO.input(pinpir))
+                        time.sleep(1)
+
+                print("    Ready")
+
+                currentstate = GPIO.input(pinpir)
+                if currentstate == 1 and previousstate == 0:
+                        streamer.log(SENSOR_LOCATION_NAME + " Movimiento", True)
+                        print("Motion detected!")
+                        r = requests.post('https://maker.ifttt.com/trigger/motion_detected/with/key/LN2n2aZaCLhO6u2IygYbV', params={"value1":"none","value2":"none","value3":"none"})
+                        previousstate = 1
+                        print("Waiting 10 seconds")
+                        time.sleep(10)
+                elif currentstate == 0 and previousstate == 1:
+                        print("Ready")
+                        previousstate = 0
+                        time.sleep(0.01)
+                streamer.flush()
+                time.sleep(60*MINUTES_BETWEEN_READS)
+
+   
         except RuntimeError:
                 print("RuntimeError, trying again...")
+                print("    Quit")
+	        GPIO.cleanup()
                 continue
-                
-        if METRIC_UNITS:
-                streamer.log(SENSOR_LOCATION_NAME + " Temperature(C)", temp_c)
-                print(temp_c)
-        else:
-                temp_f = format(temp_c * 9.0 / 5.0 + 32.0, ".2f")
-                streamer.log(SENSOR_LOCATION_NAME + " Temperature(F)", temp_f)
-        streamer.flush()
-        time.sleep(60*MINUTES_BETWEEN_READS)
