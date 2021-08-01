aa8d2c47cdc409e38e1d6094e4530b3766ea3c21
sergioR-novus <sergio@aquaexploracion.mx>
2021-08-01 15:35:11 -0500

    pir

--- app/pir2.py
+++ app/pir2.py
@@ -6,6 +6,7 @@ GPIO.setup(17, GPIO.IN)         #Read output from PIR motion sensor
 while True:
     i=GPIO.input(17)
     print(i)
+    time.sleep(1)
 if i==0:                 #When output from motion sensor is LOW
     print "No intruders",i
     time.sleep(0.1)
