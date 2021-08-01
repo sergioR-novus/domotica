43fdaa739b44ce43311f5d636f60e841cc8f8416
sergioR-novus <sergio@aquaexploracion.mx>
2021-08-01 15:29:30 -0500

    pir

--- app/pir2.py
+++ app/pir2.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(17, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
 while True:
     i=GPIO.input(17)
