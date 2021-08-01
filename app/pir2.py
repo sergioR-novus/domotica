f58c9c3642a46c0c3bbc06c35edeacbe424f312f
sergioR-novus <sergio@aquaexploracion.mx>
2021-08-01 15:28:57 -0500

    pir

--- app/pir2.py
+++ app/pir2.py
@@ -5,12 +5,12 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
 while True:
-i=GPIO.input(17)
+    i=GPIO.input(17)
 if i==0:                 #When output from motion sensor is LOW
-print "No intruders",i
-GPIO.output(3, 0)  #Turn OFF LED
-time.sleep(0.1)
+    print "No intruders",i
+    GPIO.output(3, 0)  #Turn OFF LED
+    time.sleep(0.1)
 elif i==1:               #When output from motion sensor is HIGH
-print "Intruder detected",i
-GPIO.output(3, 1)  #Turn ON LED
-time.sleep(0.1)
+    print "Intruder detected",i
+    GPIO.output(3, 1)  #Turn ON LED
+    time.sleep(0.1)
